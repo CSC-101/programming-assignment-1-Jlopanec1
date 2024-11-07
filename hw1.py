b906ad61d51f9f3d6c265bb162de3ce3d2ac6d17
Jlopanec1 <jlopanec@calpoy.edu>
2024-11-06 17:34:38 -0800

    1st

--- hw1.py
+++ hw1.py
@@ -3,26 +3,63 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+#Purpose: count every vowel in a string
+#Input: str
+#output: int
+#example = ["ever"]
+#result = 2
 
 # Part 2
+#Purpose: turn a lists of lists into smaller lists with a length of 2
+#Input: list[list[int]]
+#output: list[list[int]]
+#example = ([1,2,3], [4,5,6]
+# #result = [1,2],[3,4],[5,6]
 
 
 # Part 3
+#Purpose: remake every list of length 2 to have ascending elements, all kept in original order
+#Input: list[list[int]]
+#output: [list[list[int]]
+#example = [2,0,1],[7,5,6]
+#result = [0,1,2],[5,6,7]
 
 
 # Part 4
+#Purpose: Add the prices for two or more instances
+#Input: price:float
+#output: prince:float
+#example = ($12.49 + $13.99)
+#result = ($26.48)
 
 
 # Part 5
-
+#Purpose: calculate rectangle angle
+#Input: rectangle
+#output: int
+#example = x = 6, y = 4
+#result = 24
 
 # Part 6
+#Purpose: Show a list of books by a certain author
+#Input: str
+#output: list[book]
+#example = ["George Orwell"]
+#result = ["Animal Farm, etc"]
 
 
 # Part 7
+#Purpose: return a circle that touches the corners of the rectangle
+#Input: rectangle
+#output: circle
+#example = rectangle(3,4)
+#result = circle(diameter = 50
 
 
 # Part 8
-
+#Purpose: See who is being paid less than the average wage
+#Input: list[employee]
+#output: list[str]
+#example = ["Steve"]
+#result = [paid less]
 
--- hw1_test.py
+++ hw1_test.py
@@ -1,24 +1,56 @@
 import data
 import hw1
 import unittest
-
+class TestCases(unittest.TestCase):
 
 # Write your test cases for each part below.
 
-class TestCases(unittest.TestCase):
-    # Part 1
+    def find_vowels(sentence):
+     count = 0
+     vowels = "aeiouAEIOU"
+     for letter in sentence:
+            if letter in vowels:
+                count += 1
 
 
-    # Part 2
+
+
+
+        # Part 2
 
 
     # Part 3
 
 
     # Part 4
-
+class Price:
+    def add_prices(basket):
+        # Initialize the variable that will be used for the calculation
+        total = 0
+        # Iterate through the dictionary items
+        for item, price in basket.items():
+            # Add each price to the total calculation
+            # Hint: how do you access the values of
+            # dictionary items?
+            total += price
+        # Limit the return value to 2 decimal places
+        return round(total, 2)
+    groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59,
+                 "coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
+    print(add_prices(groceries))
 
     # Part 5
+    class Rectangle:
+        # Initialize a new Rectangle object.
+        # input: top-left corner as a Point
+        # input: bottom-right corner as a Point
+        def __init__(self, top_left: Point, bottom_right: Point):
+            self.top_left = top_left
+            self.bottom_right = bottom_right
+        def test_Rectangle_1(self):
+            rectangle = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
+            self.assertEqual(rectangle.top_left, data.Point(7, 20))
+            self.assertEqual(rectangle.bottom_right, data.Point(12, 10))
 
 
     # Part 6
